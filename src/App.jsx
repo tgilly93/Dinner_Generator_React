88a59b52d0adaf9738cf3c2998c7d1d12812c87a
Terry Gilmore <tgilly93@gmail.com>
2025-04-10 10:01:15 -0400

    TG adding in NavBar.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from './pages/HomePage.jsx';
 import MealResult from './pages/MealResult.jsx';
+import NavBar from './components/NavBar.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/MealResult" element={<MealResult />} />
