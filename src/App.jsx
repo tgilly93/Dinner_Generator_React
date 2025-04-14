4ae3eb014eae37ea21e8f28d9049be742adcc81f
Terry Gilmore <tgilly93@gmail.com>
2025-04-14 11:01:14 -0400

    TG fix bts structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import MealResult from "./pages/MealResult.jsx";
 import NavBar from "./components/NavBar.jsx";
@@ -7,18 +7,16 @@ import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <NavBar />
-        <main className="flex-grow-1">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/MealResult" element={<MealResult />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/meal/:id" element={<MealResult />} />
+        </Routes>
+      <Footer />
+    </div>
+
+        
   );
 }
 
--- src/components/MealDisplay.jsx
+++ src/components/MealDisplay.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 function MealDisplay() {
   return (
-    <div className="container">
-      <h1 className="text-center">Meal Display</h1>
-      <p className="text-center">This is the meal display page.</p>
-    </div>
+    <Container fluid>
+      <Row className="justify-content-center">
+        <Col xs={12} sm={10} md={8} lg={6}>
+          <Card className="shadow-sm">
+            <Card.Body>
+              <Card.Title className="text-center">🍽️ Meal Display</Card.Title>
+              <Card.Text className="text-center">
+                This is where the meal details will show up.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 export default MealDisplay;
